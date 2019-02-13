fa61f4fe73238744a70aad28a6ae0b4195235989
Matt Wyndham <matthewwyndham@gmail.com>
2019-02-12 23:27:24 -0700

    initialize repository

---
+++ main.py
@@ -0,0 +1,12 @@
+# Stock Market prediction with Machine Learning
+# Senior Project at BYU-I by Matt Wyndham
+
+from myapikey import APIkey
+from alpha_vantage.timeseries import TimeSeries
+import matplotlib.pyplot as plt
+
+ts = TimeSeries(key=APIkey, output_format='pandas')
+data, meta_data = ts.get_intraday(symbol='MSFT', interval='1min', outputsize='full')
+data['4. close'].plot()
+plt.title('Intraday Times Series for the MSFT stock (1 min)')
+plt.show()
