c103241c765d2b850ccd5811612c34c135a00592
Matt Wyndham <matthewwyndham@gmail.com>
2019-03-25 19:04:01 -0600

    added code to limit api access to 5 per minute for a large dataset

--- importations.py
+++ importations.py
@@ -3,9 +3,21 @@ import pandas as pd
 from myapikey import APIkey
 from alpha_vantage.timeseries import TimeSeries
 from urllib import *
-
+import csv
 
 def get_daily(stock):
     ts = TimeSeries(key=APIkey, output_format='pandas')
     data, meta_data = ts.get_daily(symbol=stock, outputsize='full')
     return data, meta_data
+
+
+def add_classification():
+    pass
+
+
+def open_csv(path_to_csv):
+    with open(path_to_csv) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            if row[0] != '':
+                print(row)
